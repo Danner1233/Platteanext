25f47e37b64134a72a6235f8abe42d69363571f1
Alexix <aorostegui2@gmail.com>
2024-08-22 19:13:44 -0500

    arreglos register con backend 22/08/2024  7:12pm

--- src/components/component/register.tsx
+++ src/components/component/register.tsx
@@ -19,7 +19,7 @@ export function Register() {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8081/api/register', {
+      const response = await fetch('http://localhost:4000/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
