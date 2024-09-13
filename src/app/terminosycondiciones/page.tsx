f9fe87f8a5e7161092b5e8ad21a8e48493a2cb92
Danner1233 <dannerariasmunive11@gmail.com>
2024-09-13 05:06:51 -0500

    Terminos y condiciones

---
+++ src/app/terminosycondiciones/page.tsx
@@ -0,0 +1,12 @@
+import { Politicas } from '@/components/component/politicas'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Politicas />
+    </div>
+  )
+}
+
+export default page
