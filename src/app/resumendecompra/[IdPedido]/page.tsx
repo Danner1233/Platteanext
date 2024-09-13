a8f4875fb82940cfee1316b396bb6845097d9ffe
Andres suarez <suarezruizandresfelipe801@gmail.com>
2024-09-13 04:31:42 -0500

    compra

---
+++ src/app/resumendecompra/[IdPedido]/page.tsx
@@ -0,0 +1,16 @@
+import { ResumenCompra } from '@/components/component/resumen-compra'
+import { Navbar } from '@/components/component/navbar';
+import { Footer } from '@/components/component/footer';
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Navbar /> 
+        <ResumenCompra />
+        <Footer />
+    </div>
+  )
+}
+
+export default page
